9be661403db9753d763aafa8664578c016348a46
gymnastiekje <70347561+gymnastiekje@users.noreply.github.com>
2020-09-10 17:37:14 +0200

    Update Berekenen vierkantsvgl 2.0.py

--- Berekenen vierkantsvgl 2.0.py
+++ Berekenen vierkantsvgl 2.0.py
@@ -1,24 +1,62 @@
+import math
+import cmath
+import fractions
 import bcolors
 
 class bcolors:
-    HEADER = '\033[95m'
-    OKBLUE = '\033[94m'
-    OKGREEN = '\033[92m'
-    WARNING = '\033[93m'
-    FAIL = '\033[91m'
-    ENDC = '\033[0m'
+    ENDC='\033[0m'
+    BOLD='\033[01m' #Maakt vet
+    DISABLE='\033[02m' #Maakt lichter
+    UNDERLINE='\033[04m' #Onderlijnt
+    REVERSE='\033[07m' #Witte achtergrond
+    STRIKETHROUGH='\033[09m'
+    INVISIBLE='\033[08m'#Maamkt onzichtbaar, duhhh
+class text:   
+    BLACK='\033[30m'
+    RED='\033[31m'
+    GREEN='\033[32m'
+    ORANGE='\033[33m'
+    BLUE='\033[34m'
+    PURPLE='\033[35m'
+    CYAN='\033[36m'
+    LIGHTGREY='\033[37m'
+    DARKGREY='\033[90m'
+    LIGHTRED='\033[91m'
+    LIGHTGREEN='\033[92m'
+    YELLOW='\033[93m'
+    LIGHTBLUE='\033[94m'
+    PINK='\033[95m'
+    LIGHTCYAN='\033[96m'
+class background:    
+    BLACK='\033[40m'
+    RED='\033[41m'
+    GREEN='\033[42m'
+    ORANGE='\033[43m'
+    BLUE='\033[44m'
+    PURPLE='\033[45m'
+    CYAN='\033[46m'
+    LIGHTGREY='\033[47m'
 
-print(bcolors.WARNING +"Interest calculator"+ bcolors.ENDC)
+print(bcolors.UNDERLINE+"To solve ax2+bx+c=0"+bcolors.ENDC)
 
-#Multiply money_start with 100 -> integers are exact, floats aren't because of the way the computer saves it. 
-money_start = int(float(input(bcolors.FAIL+"With how many money will you start?"+bcolors.ENDC))*100)
-interest = float(input(bcolors.FAIL+"What is the yearly interest?(in %)"+bcolors.ENDC))
-nYears = int(input(bcolors.FAIL+"How many years will you save the money?"+bcolors.ENDC))
+a = int(float(input("Enter the value of a:")))
+b = int(float(input("Enter the value of b:")))
+c = int(float(input("Enter the value of c:")))
 
-#devide money_end by 100 -> money is back in euros.
-money_end = (money_start*(1+(interest/100))**nYears)/100
+D = b**2-4.*a*c
+if D<0:
+   teller_real = -b 
+   noemer = 2.*a
 
-print(bcolors.OKGREEN + "After", nYears, " year(s) you'll have €", round(money_end,2), bcolors.ENDC)
+   teller_imag = math.sqrt(-D)
 
+   print("x1=", teller_real,"/", noemer, "+", teller_imag,"i /", noemer)
+   print("x2=", teller_real,"/", noemer, "-", teller_imag,"i /", noemer)
+   
+else:
+    x1 = (-b - math.sqrt(D))/(2.*a)
+    x2 = (-b + math.sqrt(D))/(2.*a)
+    print("x1=", round(x1, 2))
+    print("x2=", round(x2, 2))
 
-close = input(bcolors.OKBLUE+"Press enter to quit"+bcolors.ENDC)
+a = input("Press button to quit")
