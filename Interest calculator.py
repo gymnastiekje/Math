1b5bd5a04d04cfb8af6dd8b238c66299abf57b18
gymnastiekje <70347561+gymnastiekje@users.noreply.github.com>
2020-09-10 17:38:27 +0200

    Update Interest calculator.py

--- Interest calculator.py
+++ Interest calculator.py
@@ -1,15 +1,24 @@
-print("Interest calculator")
+import bcolors
 
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+
+print(bcolors.WARNING +"Interest calculator"+ bcolors.ENDC)
 
 #Multiply money_start with 100 -> integers are exact, floats aren't because of the way the computer saves it. 
-money_start = int(float(input("With how many money will you start?"))*100)
-interest = float(input("What is the yearly interest?(in %)"))
-nYears = int(input("How many years will you save the money?"))
+money_start = int(float(input(bcolors.FAIL+"With how many money will you start?"+bcolors.ENDC))*100)
+interest = float(input(bcolors.FAIL+"What is the yearly interest?(in %)"+bcolors.ENDC))
+nYears = int(input(bcolors.FAIL+"How many years will you save the money?"+bcolors.ENDC))
 
 #devide money_end by 100 -> money is back in euros.
 money_end = (money_start*(1+(interest/100))**nYears)/100
 
-print("After", nYears, "you'll have €", round(money_end,2))
+print(bcolors.OKGREEN + "After", nYears, " year(s) you'll have €", round(money_end,2), bcolors.ENDC)
 
 
-close = input("Press enter to quit")
+close = input(bcolors.OKBLUE+"Press enter to quit"+bcolors.ENDC)
